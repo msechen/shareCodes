961332d1cbac035b633399d7a2c54234fc77c33d
abcd <93636417@qq.com>
2021-12-17 16:35:48 +0800

    update

--- gitpush.sh
+++
@@ -1,8 +0,0 @@
-cd /jd/scripts/shareCodes/main
-git remote set-url origin git@github.com:msechen/jdrain.git
-git add redrain.json
-git commit -m "ac update"
-git push origin main
-git add redrain_half.json
-git commit -m "ac update"
-git push origin main
